987d2479214104435e39c08c8d7bfa27c58fc1ca
tobi9 <tobiasogbon9@gmail.com>
2020-07-19 00:38:25 +0100

    Fixing the Bug that affects the color selection
    I've been trying to fix the bug that affects the color selection system. I've come to find that there a problem with how the palette works and in the process of trying to fix that bug a new bug that affects the level surfaced.

--- modules/colors.lua
+++ modules/colors.lua
@@ -4,21 +4,6 @@
 -- in any script using the functions.
 local M = {}
 
-M.colors = {
-    {'red', true, vmath.vector4(1, 0, 0, 1)}, {'lime', true, vmath.vector4(0, 1, 0, 1)},
-    {'blue', true, vmath.vector4(0, 0, 1, 1)}, {'magenta', true, vmath.vector4(1, 0, 1, 1)},
-    {'yellow', true, vmath.vector4(1, 1, 0, 1)}, {'cyan', false, vmath.vector4(0, 1, 1, 1)},
-    {'gray', false, vmath.vector4(128, 128, 128, 1)}, {'maroon', false, vmath.vector4(128, 0, 0, 1)},
-    {'olive', false, vmath.vector4(128, 128, 0, 1)}, {'green', false, vmath.vector4(0, 128, 0, 1)},
-    {'purple', false, vmath.vector4(128, 0, 128, 1)}, {'teal', false, vmath.vector4(0, 128, 128, 1)},
-    {'navy', false, vmath.vector4(0, 0, 128, 1)}, {'black', false, vmath.vector4(0, 0, 0, 1)},
-    {'orange', false, vmath.vector4(255, 165, 0, 1)}, {'brown', false, vmath.vector4(165, 42, 42, 1)},
-    {'crimson', false, vmath.vector4(220, 20, 60, 1)}, {'coral', false, vmath.vector4(255, 127, 80, 1)},
-    {'olive', false, vmath.vector4(128, 128, 0, 1)}, {'khaki', false, vmath.vector4(240, 230, 140, 1)},
-    {'lemon', false, vmath.vector4(173, 248, 2, 1)}, {'violet', false, vmath.vector4(238 ,130 ,238 , 1)},
-    {'pink', false, vmath.vector4(255, 192, 103, 1)}, {'indigo', false, vmath.vector4(75 ,0 ,130 , 1)}
-}
-
 --check if the lenght of the unlocked colors
 M.check_len_of_unlkd_clrs = function()
 	local folder = "trick colors 2"
@@ -43,7 +28,7 @@ M.retrieve_colors = function()
 	local colors = sys.load(file)
     if #colors == 0 then
 		colors = {
-			--[[{'red', true, vmath.vector4(1, 0, 0, 1)}, {'lime', true, vmath.vector4(0, 1, 0, 1)},
+			{'red', true, vmath.vector4(1, 0, 0, 1)}, {'lime', true, vmath.vector4(0, 1, 0, 1)},
 			{'blue', true, vmath.vector4(0, 0, 1, 1)}, {'magenta', true, vmath.vector4(1, 0, 1, 1)},
 			{'yellow', true, vmath.vector4(1, 1, 0, 1)}, {'cyan', false, vmath.vector4(0, 1, 1, 1)},
 			{'gray', false, vmath.vector4(128, 128, 128, 1)}, {'maroon', false, vmath.vector4(128, 0, 0, 1)},
@@ -54,7 +39,7 @@ M.retrieve_colors = function()
 			{'crimson', false, vmath.vector4(220, 20, 60, 1)}, {'coral', false, vmath.vector4(255, 127, 80, 1)},
 			{'olive', false, vmath.vector4(128, 128, 0, 1)}, {'khaki', false, vmath.vector4(240, 230, 140, 1)},
 			{'lemon', false, vmath.vector4(173, 248, 2, 1)}, {'violet', false, vmath.vector4(238 ,130 ,238 , 1)},
-			{'pink', false, vmath.vector4(255, 192, 103, 1)}, {'indigo', false, vmath.vector4(75 ,0 ,130 , 1)}]]
+			{'pink', false, vmath.vector4(255, 192, 103, 1)}, {'indigo', false, vmath.vector4(75 ,0 ,130 , 1)}
         }
     end
     return colors
