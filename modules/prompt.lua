8ab67187252b332f297220706919ead5c24b0dab
tobi9 <tobiasogbon9@gmail.com>
2020-07-30 21:09:24 +0100

    Some Major Progress. Almost done with the planned rollout
    Changed the text in the challenge node to bounty instead of challenge, I thought it made it more interesting like "bounty and reward". I placed a limit on how many times the player can change the challenge after which the player spends coins to change the challenge. Changed the pause gui to look more like the menu gui, I'm to make the same changes to the gameover gui. After that i noticed that the same challenge is set after the player changes it so I increment the challenge index by one if that issue comes up but with a slight provision that if the challenge is the last in the table, I decrease it by one. But keep in mind that I haven't fully integrated the challenge system into the game environment.

--- modules/prompt.lua
+++ modules/prompt.lua
@@ -5,7 +5,7 @@
 local M = {}
 
 --remember to set to prompt_statis in the script that uses this function to false
-M.hide_prompt = function(nodes1, nodes2)
+M.prompt = function(nodes1, nodes2)
 	for k, v in ipairs(nodes1) do
 		local node = gui.get_node(v)
 		gui.set_enabled(node, false)
@@ -14,22 +14,6 @@ M.hide_prompt = function(nodes1, nodes2)
 		local node = gui.get_node(v)
 		gui.set_enabled(node, true)
 	end
-	prmpt_status = false
 end
-
---remember to set to prompt_statis in the script that uses this function to true
-M.show_prompt = function(nodes1, nodes2)
-	for k, v in ipairs(nodes1) do
-		local node = gui.get_node(v)
-		gui.set_enabled(node, true)
-	end
-
-	for k, v in ipairs(nodes2) do
-		local node = gui.get_node(v)
-		gui.set_enabled(node, false)
-	end
-    prmpt_status = true
-end
-
 
 return M
