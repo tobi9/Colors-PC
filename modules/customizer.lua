ee4102891d9dd2c81e4e570088a8fdf583945e04
tobi9 <tobiasogbon9@gmail.com>
2020-08-21 19:49:40 +0100

    Customization system progress.
    I Set the alphas in the exit prompts back to 1.
    I Deleted the prompt in the customization menu gui.
    I redesigned the customization menu gui.
    I redid the menu shell after deciding to allow customization for box nodes only.
    There's now a set all button in the shell collections.
    The customization for the menu is now fully functional.
    I removed the default colour mechanic.
    I felt it unnecessary, so I deleted the code.
    I changed "upgrades" to "shop" in the menu gui.
    The customization system for the shop is fully functional.

--- modules/customizer.lua
+++ modules/customizer.lua
@@ -9,29 +9,22 @@ local M = {}
 --is located. The color info tables are of the format {str, bool, vec4} where
 -- str is the name and identifier of the color, bool is it's unlock status and 
 --vec4 is the value of the color
-local folder = "trick colors 2"
+local folder = "trick colors 2i"
 
-M.set_node_color = function(node_id, dflt_clr_info, col)
+M.set_node_color = function(node_id, col)
 	--col and node_id are concatenated to avoid conflicts in memory location.
 	local file = sys.get_save_file(folder, col.."["..node_id.."]") --the braces are added because the gui.get_id returns the string enclosed in braces
 	local table_of_colors = sys.load(file)
 	local node_color_info
-	--pprint(table_of_colors)
 	if #table_of_colors ~= 0 then
 		node_color_info = table_of_colors
-	else
-		--set it to the default color and save it in memory
-		node_color_info = dflt_clr_info
-		sys.save(file, dflt_clr_info)
+		local node = gui.get_node(node_id)
+		gui.set_color(node, node_color_info[3])			
 	end
-	local node = gui.get_node(node_id)
-	gui.set_color(node, node_color_info[3])
-
 end
 
 M.set_new_node_color = function(node, new_clr_info, col)
 	local node_id = gui.get_id(node)
-	print(col..node_id)
 	--col and node_id are concatenated to avoid conflicts in memory location.
 	local file = sys.get_save_file(folder, col..node_id)
     gui.set_color(node, new_clr_info[3])
@@ -64,6 +57,7 @@ M.undo_change = function(ldgr)
 	local i = #ldgr
 	if i > 0 then
 		gui.set_color(ldgr[i][1], ldgr[i][2])
+		table.remove(ldgr, i)
 	end
 end
 
