f087240a755d0599d4e26f5a0ef97e6f057f4268
tobi9 <tobiasogbon9@gmail.com>
2020-11-11 17:37:00 +0100

    Animations finished and some bugs fixed
    I've finished implementing the animations in all the collections. I added more functions to the animation module and changed the scope of the variable BUTTON_ANIMATION_DURATION to the module only. I discovered that the challenge num increases if a bounty has been passed and the reward hasn't been  collected, the challenge num sti;; increases. I added a loop in the init function of the menu script to fix this.

--- modules/animation.lua
+++ modules/animation.lua
@@ -4,11 +4,24 @@
 -- in any script using the functions.
 local M = {}
 
-M.BUTTON_ANIMATION_DURATION = 0.05
+local BUTTON_ANIMATION_DURATION = 0.04
 
 function M.touch_feedback(node, hdl)
 	local pos = gui.get_position(node)
-	gui.animate(node, "position.x", pos.x + 10, gui.EASING_INOUTBOUNCE, M.BUTTON_ANIMATION_DURATION, 0, hdl, gui.PLAYBACK_ONCE_PINGPONG)
+	gui.animate(node, "position.x", pos.x + 10, gui.EASING_INOUTBOUNCE, BUTTON_ANIMATION_DURATION, 0, hdl, gui.PLAYBACK_ONCE_PINGPONG)
+end
+
+local BOUNCE_DURATION = 0.6
+function M.bounce_on_y_axis(id)
+	local node = gui.get_node(id)
+	local pos = gui.get_position(node)
+	gui.animate(node , "position.y", pos.y + 5, gui.EASING_INBACK, BOUNCE_DURATION, 0, nil, gui.PLAYBACK_LOOP_PINGPONG)
+end
+
+function M.dance_on_x_axis(id)
+	local node = gui.get_node(id)
+	local pos = gui.get_position(node)
+	gui.animate(node , "position.x", pos.x + 5, gui.EASING_INBACK, BOUNCE_DURATION, 0, nil, gui.PLAYBACK_LOOP_PINGPONG)
 end
 
 return M
